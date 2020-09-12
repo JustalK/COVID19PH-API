21eb7f3c9f5e44b943718a38c0f0ad7012a67ece
Latsuj <justal.kevin@gmail.com>
2020-09-12 22:02:41 +0800

    [FEATURE] Creating the new repository

---
+++ .xo-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "esnext": true,
+    "rules" : {
+        "camelcase": 0,
+        "no-unused-vars": 0,
+        "unicorn/filename-case": 0,
+		"ava/no-import-test-files": 0,
+		"max-params": 0
+    }
+}
---
+++ tests/tests_index.js
@@ -0,0 +1,6 @@
+const test = require('ava');
+const m = require('../src');
+
+test('[DYNAMIC] Testing the search on the different website', t => {
+	t.true(true);
+});
