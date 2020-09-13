f02b0593fd3b95feef6e7aed7c256ebf9fc9354e
Latsuj <justal.kevin@gmail.com>
2020-09-13 23:37:39 +0800

    [LINTER] Fixing linter error

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ const utils = require('./utils');
 
 module.exports = {
 	get_data: async () => {
-		//const doh_source = await google.download_DOH_report();
-		//const doh_folder_link = await pdf.get_DOH_folder_link(doh_source);
-		//const doh_folder_link = await utils.get_DOH_folder_link(doh_source);
-		//console.log(doh_folder_link);
+		const doh_source = await google.download_DOH_report();
+		const doh_folder_link = await pdf.get_DOH_folder_link(doh_source);
+		console.log(doh_folder_link);
 		const doh_folder_redirect_link = await utils.get_the_follow_link('https://bit.ly/3k4OkcG');
 		console.log(doh_folder_redirect_link);
 	}
