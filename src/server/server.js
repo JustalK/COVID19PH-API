ccd191f401ac298a9116d14563f42dbad54ef4f0
Latsuj <justal.kevin@gmail.com>
2020-09-20 21:14:43 +0800

    [FEATURE] Adding the routes

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,8 @@ const restify = require('restify');
 const mongoose = require('mongoose');
 const mongo_uri_builder = require('mongo-uri-builder');
 
+const case_model = require('./routes/cases');
+
 const db_uri = process.env.DB_URI_DATA + process.env.DB_NAME;
 const split_uri = db_uri.split('/');
 const db = split_uri[3];
@@ -21,6 +23,9 @@ mongoose.connect(db_uri_data, { useNewUrlParser: true, useUnifiedTopology: true 
 
 const server_options = {name: process.env.API_NAME}
 const server = restify.createServer(server_options);
+
+case_model(server);
+
 server.listen(process.env.PORT, process.env.HOST, function() {
 	console.log('%s listening at %s', server.name, server.url);
 });
