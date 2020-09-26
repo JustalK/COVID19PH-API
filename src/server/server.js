6ee4cab6475b221c7d2c2e1c98d4b2026511619b
Latsuj <justal.kevin@gmail.com>
2020-09-26 18:38:26 +0800

    [FEATURE] Adding a function for adding route

--- src/server/server.js
+++ src/server/server.js
@@ -2,15 +2,18 @@ const restify = require('restify');
 
 
 module.exports = {
+	adding_route: (filename_routes, server) => {
+		const route = require('./routes/' + filename_routes);
+		route(server);
+	},
 	start: () => {
-		const routes_cases = require('./routes/cases');
 
 		const server_options = {name: process.env.API_NAME}
 		const server = restify.createServer(server_options);
 
 		server.use(restify.plugins.queryParser({mapParams: true}));
 
-		routes_cases(server);
+		module.exports.adding_route('cases', server);
 
 		server.listen(process.env.PORT, process.env.HOST, function() {
 			console.log('%s listening at %s', server.name, server.url);
